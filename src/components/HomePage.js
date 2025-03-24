ce0a5dcc66961bb6bae9abfacadb4a01a1099511
unknown <pyaesoneoo54321@gmail.com>
2025-03-24 14:58:47 +1100

    added gifs and updated the result prmpts

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -5,7 +5,15 @@ const HomePage = ({ onStart }) => {
   return (
     <div className="home-page">
       <h1>How well do you know Pyae Sone?</h1>
-      <button onClick={onStart}>Start the Quiz</button>
+
+      {/* Display the GIF */}
+      <div className="gif-container">
+        <img src="/home.gif" alt="Welcome GIF" className="home-gif" />
+      </div>
+
+      <button onClick={onStart} className="start-button">
+        Start the Quiz
+      </button>
     </div>
   );
 };
